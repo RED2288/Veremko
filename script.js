dcda1e56d0b85c5e2ba978fee26c6c9d134cae5e
RED2288 <99071545+RED2288@users.noreply.github.com>
2022-02-12 10:27:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function funk1(){
+  alert('activated');
+  }
+function summ(a,b,c){
+
+return a+b+c;
+}
+/*
+var a = prompt('Введите z');
+
+if(a == 'z'){
+   funk1();
+}*/
+
+//var sum = summ(4,8,1);
+
+/*var a = parseInt(prompt('choooooo'));
+var b = parseInt(prompt('chooooook'));
+var x = parseInt(prompt('chooooool'));
+alert('summ = ' + summ(a,b,x));*/
+
+   alert('конец кода');
